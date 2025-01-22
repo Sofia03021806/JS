f9f6165170ce79b3a9b2cf748bebadda13fafc57
Sofiacat <S.sevostyanova@gmail.com>
2025-01-22 11:01:00 +0500

    Home work

---
+++ main.js
@@ -0,0 +1,8 @@
+let globalVar = "Я глобальная переменная";
+
+function printGlobal() {
+  console.log(globalVar); // Доступ к глобальной переменной
+}
+
+printGlobal(); // Вывод: Я глобальная переменная
+console.log(globalVar); // Вывод: Я глобальная переменная
